ea0ad1d69b929f4377be31c09afb5663f0d916f6
Ryan <your_email@whatever.comclear>
2018-05-24 20:40:43 -0700

    add middleware to server to account for css

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-const express = require("express");
+const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 
 var app = express();
 
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', function(req, res) {
   console.log('/');
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + '/client/index.html'));
 });
 
 
